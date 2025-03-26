83a35c289ac2296b36a2ed2dac65fdcfa012ed26
Thiago Antenor <thiagoantenor31@gmail.com>
2025-03-26 15:10:23 -0300

    feat: lab de bd tarefa 5

---
+++ disciplina_laboratorio_de_banco_de_dados/tarefa_05/exercicio_lab_bd_modelo_final_academico.sql
@@ -0,0 +1,183 @@
+CREATE DATABASE bd_academico;
+Use bd_academico;
+
+-- Criando tabelas:
+-- Tabela Pre Requisitos
+CREATE TABLE PreReq(
+	codDeptoPreReq CHAR(5),
+    numDiscPreReq INT,
+    codDepto CHAR(5),
+    numDisc	INT
+);
+
+-- Tb Disciplina
+CREATE TABLE Disciplina(
+	codDepto CHAR(5),
+    numDisc INT,
+    nomeDisc VARCHAR(10),
+    creditoDisc INT
+);
+
+-- TB Depto
+
+CREATE TABLE Depto(
+	codDepto CHAR(5),
+    nomeDepto VARCHAR(40)
+);
+
+-- Tb Professor
+CREATE TABLE Professor(
+	codProf 	INT,
+    codDepto 	CHAR(5),
+    codTit		INT,
+    nomeProf	VARCHAR(40)
+);
+
+-- TB Titulação
+CREATE TABLE Titulacao(
+	codTit INT,
+    nomeTit VARCHAR(40)
+);
+
+-- TB professorTurma
+CREATE TABLE profTurma(
+	anoSem INT,
+    codDepto CHAR(5),
+    numDisc INT,
+    siglaTur CHAR(2),
+    codProf	INT
+);
+
+-- TB Turma
+CREATE TABLE Turma(
+	anoSem INT,
+    codDepto CHAR(5),
+    numDisc	INT,
+    siglaTur CHAR(2),
+    capacTur INT
+);
+
+-- TB Horario
+CREATE TABLE Horario(
+	anoSem 		INT,
+    codDepto 	CHAR(5),
+    numDisc		INT,
+    siglaTur 	CHAR(2),
+    diaSem		INT,
+    horaInicio 	INT,
+    numSala		INT,
+    codPred		INT,
+    numHora		INT
+);
+
+-- TB Sala
+CREATE TABLE Sala(
+	codPred INT,
+    numSala INT,
+    descricaoSala VARCHAR(40),
+    capacSala INT
+);
+
+-- TB predio
+CREATE TABLE Predio(
+	codPred INT,
+    nomePred VARCHAR(40)
+);
+
+-- PRIMARY KEY E FOREIGN KEYS
+-- Depto
+ALTER TABLE Depto
+ADD CONSTRAINT PK_tb_depto
+PRIMARY KEY (codDepto);
+
+-- titulacao
+ALTER TABLE titulacao
+ADD CONSTRAINT PK_tb_titulacao
+PRIMARY KEY (codTit);
+
+-- Professor
+ALTER TABLE professor
+ADD CONSTRAINT PK_tb_professor
+PRIMARY KEY(codProf);
+
+ALTER TABLE professor
+ADD CONSTRAINT FK_tb_professor_titulacao
+FOREIGN KEY(codTit) REFERENCES titulacao(codTit);
+
+ALTER TABLE professor
+ADD CONSTRAINT FK_tb_professor_depto
+FOREIGN KEY(codDepto) REFERENCES depto(codDepto);
+
+-- Disciplina
+ALTER TABLE disciplina
+ADD CONSTRAINT PK_tb_disciplina
+PRIMARY KEY(codDepto, numDisc);
+
+ALTER TABLE disciplina
+ADD CONSTRAINT FK_tb_disciplina_tb_depto
+FOREIGN KEY(codDepto) REFERENCES depto(codDepto);
+
+-- PreReq
+ALTER TABLE PreReq
+ADD CONSTRAINT PK_tb_preReq
+PRIMARY KEY(codDeptoPreReq, numDiscPreReq, codDepto, NumDisc);
+
+ALTER TABLE PreReq
+ADD CONSTRAINT FK_PREREQ_EH_DISCIPLINA
+FOREIGN KEY(codDepto, numDisc) 
+REFERENCES Disciplina(codDepto, NumDisc);
+
+ALTER TABLE PreReq
+ADD CONSTRAINT FK_PREREQ_TEM_DISCIPLINA
+FOREIGN KEY(codDeptoPreReq, numDiscPreReq) 
+REFERENCES Disciplina(codDepto, NumDisc);
+
+-- Turma
+ALTER TABLE turma
+ADD CONSTRAINT PK_TB_Turma
+PRIMARY KEY(anoSem, CodDepto, NumDisc, SiglaTur);
+
+ALTER TABLE turma
+ADD CONSTRAINT FK_TB_TURMA_DISCIPLINA
+FOREIGN KEY(codDepto, NumDisc) REFERENCES Disciplina(codDepto, NumDisc);
+
+-- ProfTurma
+ALTER TABLE profTurma
+ADD CONSTRAINT PK_TB_prof_Turma
+PRIMARY KEY(anoSem, codDepto, numDisc, siglaTur, codProf);
+
+ALTER TABLE profTurma
+ADD CONSTRAINT FK_TB_prof_Turma_professor
+FOREIGN KEY(codProf) REFERENCES professor(codProf);
+
+ALTER TABLE profTurma
+ADD CONSTRAINT FK_TB_profTurma_Turma
+FOREIGN KEY(anoSem, codDepto, numDisc, siglaTur) REFERENCES Turma(anoSem, codDepto, numDisc, siglaTur);
+
+-- Predio
+ALTER TABLE predio
+ADD CONSTRAINT PK_TB_predio
+PRIMARY KEY (codPred);
+
+-- Sala
+ALTER TABLE Sala
+ADD CONSTRAINT PK_TB_sala
+PRIMARY KEY (codPred, NumSala);
+
+ALTER TABLE Sala
+ADD CONSTRAINT FK_TB_sala_TB_predio
+FOREIGN KEY (codPred) REFERENCES predio(codPred);
+
+-- Horario
+ALTER TABLE horario
+ADD CONSTRAINT PK_TB_horario
+PRIMARY KEY(anoSem, codDepto, numDisc, siglaTur, diaSem, horaInicio);
+
+ALTER TABLE horario
+ADD CONSTRAINT FK_TB_horario_TB_turma
+FOREIGN KEY(anoSem, codDepto, numDisc, siglaTur) REFERENCES Turma(anoSem, codDepto, numDisc, siglaTur);
+
+ALTER TABLE horario
+ADD CONSTRAINT FK_TB_horario_TB_sala
+FOREIGN KEY(codPred, numSala) REFERENCES Sala(codPred, numSala);
+
