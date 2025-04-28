c35d618e89bd66cb2e2c3b0888066e1ca21c01bf
Thiago Antenor <thiagoantenor31@gmail.com>
2025-04-28 13:37:02 -0300

    feat: envio da tarefa07

--- disciplina_laboratorio_de_banco_de_dados/tarefa_07_correcao_p1/prova_C.sql
+++ disciplina_laboratorio_de_banco_de_dados/tarefa_07_correcao_p1/prova_C.sql
@@ -1,257 +1,301 @@
+-- turma C
+
 -- TABELA MODELO
 CREATE DATABASE bd_academico;
 Use bd_academico;
-
+ 
 -- Criando tabelas:
 -- Tabela Pre Requisitos
 CREATE TABLE PreReq(
-	codDeptoPreReq CHAR(5),
+codDeptoPreReq CHAR(5),
     numDiscPreReq INT,
     codDepto CHAR(5),
-    numDisc	INT
+    numDisc INT
 );
-
+ 
 -- Tb Disciplina
 CREATE TABLE Disciplina(
-	codDepto CHAR(5),
+codDepto CHAR(5),
     numDisc INT,
     nomeDisc VARCHAR(10),
     creditoDisc INT
 );
-
+ 
 -- TB Depto
-
+ 
 CREATE TABLE Depto(
-	codDepto CHAR(5),
+codDepto CHAR(5),
     nomeDepto VARCHAR(40)
 );
-
+ 
 -- Tb Professor
 CREATE TABLE Professor(
-	codProf 	INT,
-    codDepto 	CHAR(5),
-    codTit		INT,
-    nomeProf	VARCHAR(40)
+codProf INT,
+    codDepto CHAR(5),
+    codTit INT,
+    nomeProf VARCHAR(40)
 );
-
+ 
 -- TB Titulação
 CREATE TABLE Titulacao(
-	codTit INT,
+codTit INT,
     nomeTit VARCHAR(40)
 );
-
+ 
 -- TB professorTurma
 CREATE TABLE profTurma(
-	anoSem INT,
+anoSem INT,
     codDepto CHAR(5),
     numDisc INT,
     siglaTur CHAR(2),
-    codProf	INT
+    codProf INT
 );
-
+ 
 -- TB Turma
 CREATE TABLE Turma(
-	anoSem INT,
+anoSem INT,
     codDepto CHAR(5),
-    numDisc	INT,
+    numDisc INT,
     siglaTur CHAR(2),
     capacTur INT
 );
-
+ 
 -- TB Horario
 CREATE TABLE Horario(
-	anoSem 		INT,
-    codDepto 	CHAR(5),
-    numDisc		INT,
-    siglaTur 	CHAR(2),
-    diaSem		INT,
-    horaInicio 	INT,
-    numSala		INT,
-    codPred		INT,
-    numHoras	INT
+anoSem INT,
+    codDepto CHAR(5),
+    numDisc INT,
+    siglaTur CHAR(2),
+    diaSem INT,
+    horaInicio INT,
+    numSala INT,
+    codPred INT,
+    numHoras INT
 );
-
+ 
 -- TB Sala
 CREATE TABLE Sala(
-	codPred INT,
+codPred INT,
     numSala INT,
     descricaoSala VARCHAR(40),
     capacSala INT
 );
-
+ 
 -- TB predio
 CREATE TABLE Predio(
-	codPred INT,
+codPred INT,
     nomePred VARCHAR(40)
 );
-
+ 
 -- PRIMARY KEY E FOREIGN KEYS
 -- Depto
 ALTER TABLE Depto
 ADD CONSTRAINT PK_tb_depto
 PRIMARY KEY (codDepto);
-
+ 
 -- titulacao
 ALTER TABLE titulacao
 ADD CONSTRAINT PK_tb_titulacao
 PRIMARY KEY (codTit);
-
+ 
 -- Professor
 ALTER TABLE professor
 ADD CONSTRAINT PK_tb_professor
 PRIMARY KEY(codProf);
-
+ 
 ALTER TABLE professor
 ADD CONSTRAINT FK_tb_professor_titulacao
 FOREIGN KEY(codTit) REFERENCES titulacao(codTit);
-
+ 
 ALTER TABLE professor
 ADD CONSTRAINT FK_tb_professor_depto
 FOREIGN KEY(codDepto) REFERENCES depto(codDepto);
-
+ 
 -- Disciplina
 ALTER TABLE disciplina
 ADD CONSTRAINT PK_tb_disciplina
 PRIMARY KEY(codDepto, numDisc);
-
+ 
 ALTER TABLE disciplina
 ADD CONSTRAINT FK_tb_disciplina_tb_depto
 FOREIGN KEY(codDepto) REFERENCES depto(codDepto);
-
+ 
 -- PreReq
 ALTER TABLE PreReq
 ADD CONSTRAINT PK_tb_preReq
 PRIMARY KEY(codDeptoPreReq, numDiscPreReq, codDepto, NumDisc);
-
+ 
 ALTER TABLE PreReq
 ADD CONSTRAINT FK_PREREQ_EH_DISCIPLINA
-FOREIGN KEY(codDepto, numDisc) 
+FOREIGN KEY(codDepto, numDisc)
 REFERENCES Disciplina(codDepto, NumDisc);
-
+ 
 ALTER TABLE PreReq
 ADD CONSTRAINT FK_PREREQ_TEM_DISCIPLINA
-FOREIGN KEY(codDeptoPreReq, numDiscPreReq) 
+FOREIGN KEY(codDeptoPreReq, numDiscPreReq)
 REFERENCES Disciplina(codDepto, NumDisc);
-
+ 
 -- Turma
 ALTER TABLE turma
 ADD CONSTRAINT PK_TB_Turma
 PRIMARY KEY(anoSem, CodDepto, NumDisc, SiglaTur);
-
+ 
 ALTER TABLE turma
 ADD CONSTRAINT FK_TB_TURMA_DISCIPLINA
 FOREIGN KEY(codDepto, NumDisc) REFERENCES Disciplina(codDepto, NumDisc);
-
+ 
 -- ProfTurma
 ALTER TABLE profTurma
 ADD CONSTRAINT PK_TB_prof_Turma
 PRIMARY KEY(anoSem, codDepto, numDisc, siglaTur, codProf);
-
+ 
 ALTER TABLE profTurma
 ADD CONSTRAINT FK_TB_prof_Turma_professor
 FOREIGN KEY(codProf) REFERENCES professor(codProf);
-
+ 
 ALTER TABLE profTurma
 ADD CONSTRAINT FK_TB_profTurma_Turma
 FOREIGN KEY(anoSem, codDepto, numDisc, siglaTur) REFERENCES Turma(anoSem, codDepto, numDisc, siglaTur);
-
+ 
 -- Predio
 ALTER TABLE predio
 ADD CONSTRAINT PK_TB_predio
 PRIMARY KEY (codPred);
-
+ 
 -- Sala
 ALTER TABLE Sala
 ADD CONSTRAINT PK_TB_sala
 PRIMARY KEY (codPred, NumSala);
-
+ 
 ALTER TABLE Sala
 ADD CONSTRAINT FK_TB_sala_TB_predio
 FOREIGN KEY (codPred) REFERENCES predio(codPred);
-
+ 
 -- Horario
 ALTER TABLE horario
 ADD CONSTRAINT PK_TB_horario
 PRIMARY KEY(anoSem, codDepto, numDisc, siglaTur, diaSem, horaInicio);
-
+ 
 ALTER TABLE horario
 ADD CONSTRAINT FK_TB_horario_TB_turma
 FOREIGN KEY(anoSem, codDepto, numDisc, siglaTur) REFERENCES Turma(anoSem, codDepto, numDisc, siglaTur);
-
+ 
 ALTER TABLE horario
 ADD CONSTRAINT FK_TB_horario_TB_sala
 FOREIGN KEY(codPred, numSala) REFERENCES Sala(codPred, numSala);
 /*------------------------------------------------------------*/
-
--- Inserção de dados
+ 
+-- INSERTS
+-- Departamento
 INSERT INTO depto (codDepto, nomeDepto) VALUES
-('0001A', 'Ciencia da Computação'),
-('0002A', 'Recursos Humanos'),
-('0003A', 'Logistica'),
 ('INF01', 'Administração'),
 ('INF02', 'Informática');
-
-
+ 
+-- Disciplina
 INSERT INTO disciplina (codDepto, numDisc, nomeDisc, creditoDisc) VALUES
-('0001A', 1, 'LPEA', 100),
-('0001A', 2, 'LP', 200),
-('0002A', 1, 'CTB', 100),
-('INF01', 1, 'GTE', 500),
+('INF02', 1, 'GTE', 500),
 ('INF01', 3, 'ADM', 300),
 ('INF01', 4, 'SI', 300),
 ('INF02', 5, 'AMM', 50),
-('INF02', 6, 'TESTE', 100),
-('INF01', 6, 'GPP', 100);
-
-INSERT INTO preReq (codDeptoPreReq, numDiscPreReq, codDepto, numDisc) VALUES
-('INF01', 3, 'INF01', 4),
-('INF01', 3, 'INF02', 6),
-('INF01', 4, 'INF02', 6);
-
+('INF02', 2, 'TESTE', 100),
+('INF02', 6, 'GPP', 100);
+ 
 INSERT INTO turma (anoSem, codDepto, numDisc, siglaTur, capacTur) VALUES
-(20241,'0001A', 1, 'TA', 40),
-(20242, '0001A', 1, 'TB', 40),
 (20021, 'INF01', 3, 'TN', 30),
 (20021, 'INF01', 4, 'TN', 30),
 (20021, 'INF02', 5, 'TC', 30),
-(20022, 'INF01', 1, 'TA', 30),
-(20021, 'INF02', 6, 'TA', 30),
-(20021, 'INF02', 6, 'TA', 30),
-(20021, 'INF01', 6, 'TX', 30);
-
+(20022, 'INF02', 1, 'TA', 30),
+(20021, 'INF02', 2, 'TA', 30),
+(20021, 'INF02', 6, 'TN', 30),
+(20021, 'INF02', 6, 'TX', 30);
+ 
 INSERT INTO titulacao(CodTit, NomeTit) VALUES
 (1, 'Mestre'),
 (2, 'Doutor');
-
+ 
 INSERT INTO professor (codProf, codDepto, codTit, nomeProf) VALUES
-(1, '0001A', 1, 'Geralt de Rivia'),
 (2, 'INF01', 2, 'Yenneffer'),
 (3, 'INF01', 2, 'Antunes'),
 (4, 'INF01', 2, 'Jessica');
-
+ 
 INSERT INTO profTurma (anoSem, codDepto, numDisc, siglaTur, codProf) VALUES
 (20021, 'INF01', 3, 'TN', 2),
 (20021, 'INF01', 4, 'TN', 3),
 (20021, 'INF02', 5, 'TC', 4),
-(20022, 'INF01', 1, 'TA', 4),
-(20021, 'INF02', 6, 'TA', 3),
+(20022, 'INF02', 1, 'TA', 4),
+(20021, 'INF02', 2, 'TA', 3),
 (20021, 'INF02', 6, 'TN', 3),
-(20021, 'INF01', 6, 'TX', 2);
-
+(20021, 'INF02', 6, 'TX', 2);
+ 
 INSERT INTO predio (codPred, nomePred) VALUES
-(1, 'Principal'),
-(43423, 'Laboratorios');
-
+(1, 'Principal');
+ 
 INSERT INTO sala (codPred, numSala, descricaoSala, capacSala) VALUES
-(1, 12, 'sala de aula padrão', 40),
-(1, 22, 'sala de aula padrão', 35),
-(43423, 101, 'lab com notebooks',35);
-
+(1, 9, 'Salão Nobre', 50);
+ 
+ 
 INSERT INTO horario(anoSem, codDepto, numDisc, siglaTur, diaSem, horaInicio, numSala, codPred, numHoras) VALUES
-(20022, 'INF01', 1, 'TA', 2, 1450, 22, 1, 4),
-(20021, 'INF01', 3, 'TN', 3, 1900, 12, 1, 4),
-(20021, 'INF01', 4, 'TN', 4, 1900, 12, 1, 4),
-(20021, 'INF02', 5, 'TC', 2, 1900, 22, 1, 2),
-(20021, 'INF02', 6, 'TA', 2, 0800, 12, 1, 2),
-(20021, 'INF02', 6, 'TN', 6, 1900, 101, 43423, 4),
-(20021, 'INF01', 6, 'TX', 3, 1900, 12, 1, 4);
+(20021, 'INF01', 3, 'TN', 3, 1900, 9, 1, 4),
+(20021, 'INF01', 4, 'TN', 4, 1900, 9, 1, 4),
+(20021, 'INF02', 5, 'TC', 2, 1900, 9, 1, 2),
+(20022, 'INF02', 1, 'TA', 2, 0800, 9, 1, 2),
+(20021, 'INF02', 6, 'TN', 6, 1900, 9, 1, 4),
+(20021, 'INF02', 6, 'TX', 3, 1900, 9, 1, 4);
+-- CRIAR PROCEDURE COM APENAS UMA QUERY DE CURSOR PARA SELECIONAR: Turma C : O nome das disciplinas 
+-- e o nome do professor para as turmas cujo a descrição da sala seja 'Salão Nobre'. 
+-- Listar no máximo 3 disciplinas do Departamento de Informática.
+
+DELIMITER $$
+CREATE PROCEDURE obter_nomeDisc_nomeProf_salao_Nobre()
+BEGIN
+DECLARE final BOOLEAN DEFAULT FALSE;
+    DECLARE _nomeDisc VARCHAR(10);
+    DECLARE _nomeProf VARCHAR(40);
+    DECLARE _nomeDepto VARCHAR(40);
+    DECLARE contadorCod INT DEFAULT 0;
+    
+ 
+DECLARE nomeCursor CURSOR FOR
+	SELECT  d.nomeDisc, p.nomeProf, dep.nomeDepto from professor p
+	INNER JOIN profTurma profT ON profT.codProf = p.codProf
+	INNER JOIN turma t ON profT.anoSem = t.anoSem
+	   AND profT.codDepto = t.codDepto
+	   AND profT.numDisc = t.numDisc
+	   AND profT.siglaTur = t.siglaTur
+	INNER JOIN disciplina d ON t.codDepto = d.codDepto
+	AND t.numDisc = d.numDisc
+	INNER JOIN horario h ON h.anoSem = t.anoSem
+			AND h.codDepto = t.codDepto
+			AND h.numDisc = t.numDisc
+			AND h.siglaTur = t.siglaTur
+	INNER JOIN sala s ON h.codPred = s.codPred
+	  AND h.numSala = s.numSala
+	INNER JOIN depto dep ON d.codDepto = dep.codDepto
+	WHERE s.descricaoSala = 'Salão Nobre';
+	
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET final = TRUE;
+    
+    CREATE TEMPORARY TABLE tb_nome( t_nomeDisc VARCHAR(10),t_nomeProf VARCHAR(10), t_nomeDepto VARCHAR(40));
+-- Executa o CURSOR
+    OPEN nomeCursor;
+    
+    laco_leitura: LOOP
+FETCH nomeCursor INTO _nomeDisc, _nomeProf, _nomeDepto;
+        IF final THEN
+			LEAVE laco_leitura;
+		END IF;
+        IF _nomeDepto = 'Informática' THEN
+				IF contadorCod < 3 THEN
+					SET contadorCod = contadorCod + 1;
+					INSERT INTO tb_nome (t_nomeDisc, t_nomeProf, t_nomeDepto) VALUES (_nomeDisc, _nomeProf, _nomeDepto);
+				END IF;
+        ELSE 
+			INSERT INTO tb_nome (t_nomeDisc, t_nomeProf, t_nomeDepto) VALUES (_nomeDisc, _nomeProf, _nomeDepto);
+        END IF;
+END LOOP;
+    SELECT t_nomeDisc, t_nomeProf FROM tb_nome;
+    DROP TEMPORARY TABLE tb_nome;
+    CLOSE nomeCursor;
+END$$
+DELIMITER ;
+CALL obter_nomeDisc_nomeProf_salao_Nobre();
